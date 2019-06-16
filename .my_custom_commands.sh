f51b676ff99a7d021de3ca28ced072ab3e95481b
Lucas <lmagalhaes1126@gmail.com>
2019-06-16 00:33:59 -0400

    Added functionality of ConfigFile to the bash script too

--- .my_custom_commands.sh
+++ .my_custom_commands.sh
@@ -8,23 +8,22 @@ function create_project() {
     else
         CONFIG_FILE=~/.create_github_repo_config.json
         REPO=$(python3 .create_github_repo.py $1 $2)
-    
+    fi
     echo $CONFIG_FILE
-    PROJS_FOLDER=$(python3 -c "import json; print json.load($CONFIG_FILE)['project_folder']")
+    PROJS_FOLDER=$(python3 -c "import json; print(json.load($CONFIG_FILE)['project_folder']")
     echo $PROJS_FOLDER
-    CURR_PROJ_FOLDER="$PROJ_FOLDER$1"
-    
-    # if [ "$REPO" = "ERROR" ]; then
-    #     echo error found, check log
-    # else
-    #     echo $REPO
-    #     cd $CURR_PROJ_FOLDER
-    #     git clone $REPO .
-    #     git branch -a
-    #     touch .gitignore
-    #     git add -A
-    #     git commit -m "Initial Commit with .gitignore"
-    #     git push origin master
-    # fi
+    CURR_PROJ_FOLDER='$PROJ_FOLDER$1'
 
+    if [ "$REPO" = "ERROR" ]; then
+            echo error found, check log
+        else
+            echo $REPO
+            cd $CURR_PROJ_FOLDER
+            git clone $REPO .
+            git branch -a
+            touch .gitignore
+            git add -A
+            git commit -m "Initial Commit with .gitignore"
+            git push origin master
+        fi
 }
